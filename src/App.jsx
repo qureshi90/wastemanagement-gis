a6ada4af87e2db04ff885ef41b437011f56041d0
Waqar Qureshi <waqarqureshi90@gmail.com>
2023-05-18 15:58:45 +0500

    loading screen with animation and favicon added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
-import './App.css';
-import WasteManagementMap from './Components/WasteManagementMap';
-import RegisterUser from './Components/RegisterUser';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import WasteManagementMap from "./Components/WasteManagementMap";
+import RegisterUser from "./Components/RegisterUser";
+import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   const router = createBrowserRouter([
     {
-      path: '/',
+      path: "/",
       element: <RegisterUser />,
     },
     {
-      path: '/map',
+      path: "/map",
       element: <WasteManagementMap />,
     },
   ]);
-  return <RouterProvider router={router} />;
+
+  return loading ? (
+    <div className="w-screen h-screen">
+      <img
+        src={require("./assets/image.jpeg")}
+        className="w-full h-full"
+        alt="loading"
+      />
+      <div className="linear-loader" />
+    </div>
+  ) : (
+    <RouterProvider router={router} />
+  );
 }
 
 export default App;
